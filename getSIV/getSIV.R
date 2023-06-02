2447ebaab9157a0df4c620bdce7f96dbd77d5a3f
Georgios Boumis <62819177+bgeorgios@users.noreply.github.com>
2023-06-02 12:33:03 -0500

    Update getSIV.R

--- getSIV/getSIV.R
+++ getSIV/getSIV.R
@@ -50,14 +50,14 @@ for (f in rain.files) {
               nrow = dim(rain)[1],
               ncol = dim(rain)[2])
   mat[is.na(mat)] <- 0.0
-  write(c(step, dim(mat)[1], dim(mat)[2]),
+  write(c(step, dim(mat)[2], dim(mat)[1]),
         "rain.dat",
         ncol = 3,
         append = TRUE)
-  for (r in 1:ncol(mat)) {
-    write(mat[,r],
+  for (r in 1:nrow(mat)) {
+    write(mat[r,],
           "rain.dat",
-          ncol = dim(mat)[1],
+          ncol = dim(mat)[2],
           append = TRUE)
   }
   step <- step + 3600
